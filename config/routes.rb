9da7b85698768f317f31995e695630788747fcbe
timhdavis <timhdavis@yahoo.com>
2018-09-18 16:03:56 -0700

    added Volunteers as a Resource in Routes
    -added Volunteers as a "resouce" in the routes config to create basic routes.
    -can use "rake routes" to check all routes created when marked as a resource.

--- config/routes.rb
+++ config/routes.rb
@@ -4,7 +4,11 @@ Rails.application.routes.draw do
 
   get 'welcome/index'
 
-  get 'volunteers/index'
+  #get 'volunteers/index'
+
+  # Resources: (maps HTTP verbs to Controller actions)
+
+  resources :volunteers
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
