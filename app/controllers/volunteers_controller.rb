2709a1be3d328d08ed33ea38711a480c6e1fd047
timhdavis <timhdavis@yahoo.com>
2018-09-18 16:08:13 -0700

    added New Volunteer form
    -added a "new" method in VolunteeersController.
    -added a simple form in the New Volunteer page. Form fields include "first name", "last name", "email address", and "notes".
    -Note: not connected to a model or the database yet.

--- app/controllers/volunteers_controller.rb
+++ app/controllers/volunteers_controller.rb
@@ -3,6 +3,24 @@ class VolunteersController < ApplicationController
     layout false
 
     def index
+        #
     end
+
+    def show
+        #
+    end
+
+    def new
+        #
+    end
+
+    def create
+        # for testing: render plain: params[:volunteer].inspect
+
+
+
+    end
+
+
 
 end
