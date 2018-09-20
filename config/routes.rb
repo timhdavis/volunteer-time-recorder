aa0ef6e29048e6b1754aaad474434acb2b7352ff
timhdavis <timhdavis@yahoo.com>
2018-09-20 11:34:25 -0700

    added delete route path for volunteers
    -modified volunteer resource definition to add the "delete" action (not included by default in Rails).
    -adding this will allow us to use the "delete_volunteer_path" if we want to send the user to the Delete Volunteer page (a confirmation page) instead of directly going to the "destroy" action.
    -the Delete Volunteer page is currently not being used.

--- config/routes.rb
+++ config/routes.rb
@@ -8,7 +8,12 @@ Rails.application.routes.draw do
 
   # Resources: (maps HTTP verbs to Controller actions)
 
-  resources :volunteers
+  resources :volunteers do
+    member do
+      get :delete # Add delete action; not added by default in Rails.
+          # This lets us use "delete_volunteer_path".
+    end
+  end
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
