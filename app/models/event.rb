9d59465315fa4b41e5e940e14a3aec66b4c0ec5b
timhdavis <timhdavis@yahoo.com>
2018-10-06 13:21:45 -0700

    added event title as a method to event model
    -added "event_title" method to Event model that displays the event name and year in the form:
    --"Rummage Sale (2019)"

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,3 +1,10 @@
 class Event < ApplicationRecord
     has_many :time_records, :dependent => :delete_all
+
+    # Other methods:
+
+    def event_title
+        return (self.name.to_s + " (" + self.year.to_s + ")");
+    end
+
 end
